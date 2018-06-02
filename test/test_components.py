c1d38181acc99fbdaade6f8e1457a7d259ef93dd
Dan Liberatori <garroad_ran@hotmail.com>
2018-06-03 06:09:40 +0900

    Add clone function to Card

--- test/test_components.py
+++ test/test_components.py
@@ -6,16 +6,26 @@ class TestCard(unittest.TestCase):
         """Returns a tuple containing suit and number"""
         c = Card(4, 'basto')
         self.assertEqual((4, 'basto'), c.info())
-        
+
         # Check that an error is raised on an invalid number
         with self.assertRaises(ValueError):
             c = Card(13, 'espada')
+
+
+    def test_clone(self):
+        """Clones a card"""
+        c = Card(1, 'oro')
+        d = c.clone()
+
+        # Modifying this private attribute just for the purposes of testing
+        c._number = 3
+        self.assertEqual((1, 'oro'), d.info())
 
     @unittest.skip("Not implemented")
     def test_image(self):
         """Returns an image for the card"""
         c = Card(10, 'oro')
-        
+
         self.fail('Failed')
 
 class TestDeck(unittest.TestCase):
@@ -23,7 +33,7 @@ class TestDeck(unittest.TestCase):
         """Takes a Card class and builds a deck of 40 Cards"""
         deck = Deck(Card)
         self.assertEqual(40, len(deck.cards()))
-        
+
         # Assert that there are no duplicate cards in the deck
         deck_check = {'oro': [], 'espada': [], 'copa': [], 'basto': []}
         for card in deck.cards():
@@ -32,27 +42,27 @@ class TestDeck(unittest.TestCase):
                 self.fail('Duplicate card in deck')
             else:
                 deck_check[palo].append(number)
-        
+
         # And that each suit has cards 1-10
         for palo in deck_check:
             for i in range(1, 11):
                 if not i in deck_check[palo]:
                     self.fail('Missing ' + str(i) + ' in ' + palo)
-        
-    
+
+
     def test_cards(self):
         """Returns a copy of the deck"""
         deck = Deck(Card)
         cards = deck.cards()
         cards.append('More cards!')
         self.assertEqual(40, len(deck.cards()))
-        
+
     def test_deal(self):
         """Deals a random set of cards, and removes them from the deck"""
         deck = Deck(Card)
         deck.deal(4)
         self.assertEqual(36, len(deck.cards()))
-        
+
         deck.deal(3)
         self.assertEqual(33, len(deck.cards()))
 
@@ -65,7 +75,7 @@ class TestPlayer(unittest.TestCase):
         c = Card(5, 'oro')
         bob.pick_up_hand([a, b, c])
         self.assertEqual([a, b, c], bob.current_hand())
-    
+
     def test_name(self):
         """Getter for the player's name"""
         alice = Player('Alice')
@@ -78,17 +88,17 @@ class TestPlayer(unittest.TestCase):
         b = Card(4, 'oro')
         c = Card(5, 'espada')
         alice.pick_up_hand([a, b, c])
-        
+
         self.assertTrue(alice.holds_card((3, 'oro')))
         self.assertTrue(alice.holds_card((5, 'espada')))
         self.assertFalse(alice.holds_card((10, 'oro')))
         self.assertFalse(alice.holds_card((4, 'copa')))
-    
+
     def test_total_score(self):
         """Getter for the player's score"""
         alice = Player('Alice')
         self.assertEqual(0, alice.total_score())
-    
+
     def test_place_card_on_mesa(self):
         """Puts down a card on the mesa"""
         alice = Player('Alice')
@@ -96,12 +106,12 @@ class TestPlayer(unittest.TestCase):
         card2 = Card(10, 'basto')
         card3 = Card(8, 'espada')
         alice.pick_up_hand([card1, card2, card3])
-        
+
         mesa = []
         alice.place_card_on_mesa(mesa, (5, 'oro'))
         self.assertEqual(card1, mesa[0])
         self.assertEqual([card2, card3], alice.current_hand())
-        
+
         alice.place_card_on_mesa(mesa, (8, 'espada'))
         self.assertEqual([card1, card3], mesa)
         self.assertEqual([card2], alice.current_hand())
@@ -111,7 +121,7 @@ class TestPlayer(unittest.TestCase):
         cards from the mesa, adding up to 15, and places them
         in their pila. If last card is taken from the mesa,
         an escoba is counted.
-        
+
         Modifies the player's currenthand, pila, and the mesa.
         """
         alice = Player('Alice')
@@ -129,12 +139,12 @@ class TestPlayer(unittest.TestCase):
         alice.pick_up_hand([card_alice1, card_alice2, card_alice3])
         bob.pick_up_hand([card_bob1, card_bob2, card_bob3])
         mesa = [card_mesa1, card_mesa2, card_mesa3]
-        
+
         # Alice uses her 7 to pick up two 4s from the table
         alice.pick_up_from_mesa(mesa, (7, 'basto'), [(4, 'basto'), (4,'copa')])
         self.assertEqual([card_mesa3], mesa)
         self.assertEqual([card_alice1, card_alice3], alice.current_hand())
-        
+
         # Next, bob uses his 5 to pick up the 10 from the table and get an escoba
         bob.pick_up_from_mesa(mesa, (5, 'espada'), [(10, 'oro')])
         self.assertEqual([], mesa)
@@ -150,7 +160,7 @@ class TestPila(unittest.TestCase):
         oro = Card(7, 'oro')
         espada = Card(10, 'espada')
         copa = Card(1, 'copa')
-        
+
         pila1.add([basto, oro, espada, copa])
         pila2.add([basto, oro, espada])
 
@@ -164,7 +174,7 @@ class TestPila(unittest.TestCase):
         card2 = Card(6, 'basto')
         card3 = Card(6, 'oro')
         card4 = Card(1, 'espada')
-        
+
         pila.add([card1, card2])
         current_cards = pila.get_cards()
         self.assertEqual(2, len(current_cards['basto']))
@@ -199,15 +209,15 @@ class TestPila(unittest.TestCase):
         cards = pila.get_cards()
         # modifying the dictionary doesn't actually affect the pila
         cards['basto'].append(card3)
-        
+
         pilaCards = pila.get_cards()
         self.assertEqual(2, len(pilaCards['basto']))
-        
+
     def test_total_cards(self):
         """Counts the total number of cards the player has picked up.Counts"""
         pila = Pila()
         self.assertEqual(0, pila.total_cards())
-        
+
         card1 = Card(9, 'basto')
         card2 = Card(6, 'basto')
         card3 = Card(5, 'espada')
@@ -216,13 +226,13 @@ class TestPila(unittest.TestCase):
         card6 = Card(5, 'espada')
         pila.add([card1, card2])
         self.assertEqual(2, pila.total_cards())
-        
+
         pila.add([card3])
         self.assertEqual(3, pila.total_cards())
 
         pila.add([card4, card5, card6])
         self.assertEqual(6, pila.total_cards())
-    
+
     def test_total_oros(self):
         """Counts the total number of oros collected by the player"""
         pila = Pila()
@@ -234,13 +244,13 @@ class TestPila(unittest.TestCase):
         card5 = Card(10, 'copa')
         pila.add([card1, card2])
         self.assertEqual(0, pila.total_oros())
-        
+
         pila.add([card3])
         self.assertEqual(1, pila.total_oros())
-        
+
         pila.add([card4, card5])
         self.assertEqual(2, pila.total_oros())
-        
+
     def test_has_siete_de_belo(self):
         """Checks whether the 7 de belo is in the pile"""
         pila = Pila()
@@ -250,13 +260,13 @@ class TestPila(unittest.TestCase):
         card4 = Card(7, 'oro')
         card5 = Card(8, 'oro')
         card6 = Card(7, 'espada')
-        
+
         pila.add([card1, card2, card3])
         self.assertFalse(pila.has_siete_de_belo())
-        
+
         pila.add([card4])
         self.assertTrue(pila.has_siete_de_belo())
-        
+
         pila.add([card5, card6])
         self.assertTrue(pila.has_siete_de_belo())
 
