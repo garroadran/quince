aa97548ae9b3856663a0534673bd8c31fd0d661d
garroadran <garroad_ran@hotmail.com>
2018-09-30 10:29:52 +0900

    Add setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open('README.md' 'r') as fh:
+    long_description = fh.read()
+
+setup(name='Quince',
+      version='1.0',
+      description='Classic Spanish card game.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='Dan Liberatori',
+      author_email='daniel.liberatori@gmail.com',
+      url='http://www.danliberatori.com',
+      packages=['quince', 'ui']
+     )
